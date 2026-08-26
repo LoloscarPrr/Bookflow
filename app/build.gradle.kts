2bb874d23d016a36ef8f0b91423a08e0de9861e1
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-26 11:13:30 -0400

    build: bump alpha10 narration cache

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.bookflow.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.2.0-alpha09.1"
+        versionCode = 14
+        versionName = "0.2.0-alpha10"
     }
 
     compileOptions {
